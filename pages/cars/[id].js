4f02bf6bf65ffcefbe2e6ccd88682b4b65fff93c
JPaul23 <73589069+JPaul23@users.noreply.github.com>
2022-05-29 16:37:00 +0200

    Server-side rendering

--- pages/cars/[id].js
+++ pages/cars/[id].js
@@ -16,16 +16,26 @@ export default function Car({ car }) {
     )
 }
 
-export async function getStaticProps({ params }) {
+/* does everything as the static-generation function but on every request instead on build time. */
+export async function getServerSideProps({ params }) {
+    const req = await fetch(`http://localhost:3000/${params.id}.json`);
+    const data = await req.json();
+
+    return {
+        props: { car: data }
+    };
+}
+
+/* export async function getStaticProps({ params }) {
     const req = await fetch(`http://localhost:3000/${params.id}.json`);
     const data = await req.json();
 
     return {
         props: { car: data }
     }
-}
+} */
 
-export async function getStaticPaths() {
+/* export async function getStaticPaths() {
     const req = await fetch(`http://localhost:3000/cars.json`);
     const data = await req.json();
 
@@ -38,4 +48,4 @@ export async function getStaticPaths() {
         paths,
         fallback: false
     };
-}
+} */
